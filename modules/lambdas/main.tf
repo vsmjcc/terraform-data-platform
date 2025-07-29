440cb3cfd3e744cf0dcb887c23b0f6cf0e1270f4
Isabella Tessarotto <isabella.tessarotto@musa.co>
2025-07-29 15:20:44 -0300

    adicionando lambdas shopify

--- modules/lambdas/main.tf
+++ modules/lambdas/main.tf
@@ -1,6 +1,26 @@
 module "typeform_lambda" {
   source             = "../lambda_function"
-  function_name      = "typeform-webhook"
+  function_name      = "ingest-typeform-to-bronze"
+  handler            = "main.handler"
+  runtime            = "python3.11"
+  timeout            = 10
+  memory_size        = 128
+
+  s3_bucket          = var.packages_bucket_name
+  s3_key             = "empty-lambda.zip"
+
+  enable_http_api    = true
+  public_access      = true
+
+  environment_variables = {
+    STAGE     = var.environment
+    LOG_LEVEL = "debug"
+  }
+}
+
+module "shopify_lambda" {
+  source             = "../lambda_function"
+  function_name      = "ingest-shopify-to-bronze"
   handler            = "main.handler"
   runtime            = "python3.11"
   timeout            = 10
@@ -22,5 +42,6 @@ module "typeform_lambda" {
 locals {
   lambda_modules = {
     typeform   = module.typeform_lambda
+    shopify    = module.shopify_lambda
   }
 }
