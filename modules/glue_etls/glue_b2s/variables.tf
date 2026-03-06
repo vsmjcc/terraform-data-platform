6ea49a1b29e63e0f14ff15cae777aaabfe4538ec
Ana Beatriz <ana.vieira@zerezes.com.br>
2026-03-06 09:44:09 -0300

    refactor(terraform): wire glue_b2s module into glue_etls

---
+++ modules/glue_etls/glue_b2s/variables.tf
@@ -0,0 +1,39 @@
+variable "environment" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+# Se já existir um SG para Glue, passe aqui. Se null, este módulo cria um.
+variable "glue_sg_id" {
+  type    = string
+  default = null
+}
+
+# Buckets (sem ARN)
+variable "bronze_bucket" {
+  type = string # ex: "zrzs-dev-data-lake-bronze"
+}
+
+variable "silver_bucket" {
+  type = string # ex: "zrzs-dev-data-lake-silver"
+}
+
+variable "gold_bucket" {
+  type = string # ex: "zrzs-dev-data-lake-gold"
+}
+
+variable "etls_bucket" {
+  type = string # ex: "zrzs-dev-etls"
+}
+
+variable "common_tags" {
+  type    = map(string)
+  default = {}
+}
