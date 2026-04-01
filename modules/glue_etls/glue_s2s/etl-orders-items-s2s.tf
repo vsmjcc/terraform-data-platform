5de268ccd9c4ba9a76e518b2119ad7f0336336cb
Ana Beatriz <ana.vieira@zerezes.com.br>
2026-04-01 13:39:56 -0300

    fix: replace local glue security group reference with variable

--- modules/glue_etls/glue_s2s/etl-orders-items-s2s.tf
+++ modules/glue_etls/glue_s2s/etl-orders-items-s2s.tf
@@ -14,7 +14,7 @@ module "glue_job_orders_items_s2s_enriched" {
 
   use_vpc            = true
   subnet_ids         = var.private_subnet_ids
-  security_group_ids = [local.glue_sg_id]
+  security_group_ids = [var.glue_sg_id]
 
   default_arguments = {
     "--SHOPIFY_GLUE_DATABASE"  = "silver_commerce"
