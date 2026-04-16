0fc0dd499c79d77bb70c2e1e6130459826709f1b
Isabella Tessarotto <isabella.tessarotto@zerezes.com.br>
2026-04-16 19:10:46 -0300

    fix remove lambdas

--- modules/iam/attachments.tf
+++ modules/iam/attachments.tf
@@ -39,20 +39,20 @@ resource "aws_iam_role_policy_attachment" "attach_bronze_write_to_lambda" {
   policy_arn = aws_iam_policy.bronze_write_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "attach_shopify_orders_bronze_write_to_lambda" {
-  role       = "ingest-shopify-orders-to-bronze-lambda-role"
-  policy_arn = aws_iam_policy.bronze_write_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach_shopify_orders_bronze_write_to_lambda" {
+#   role       = "ingest-shopify-orders-to-bronze-lambda-role"
+#   policy_arn = aws_iam_policy.bronze_write_policy.arn
+# }
 
-resource "aws_iam_role_policy_attachment" "attach_shopify_products_bronze_write_to_lambda" {
-  role       = "ingest-shopify-products-to-bronze-lambda-role"
-  policy_arn = aws_iam_policy.bronze_write_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach_shopify_products_bronze_write_to_lambda" {
+#   role       = "ingest-shopify-products-to-bronze-lambda-role"
+#   policy_arn = aws_iam_policy.bronze_write_policy.arn
+# }
 
-resource "aws_iam_role_policy_attachment" "attach_shopify_customers_bronze_write_to_lambda" {
-  role       = "ingest-shopify-customers-to-bronze-lambda-role"
-  policy_arn = aws_iam_policy.bronze_write_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach_shopify_customers_bronze_write_to_lambda" {
+#   role       = "ingest-shopify-customers-to-bronze-lambda-role"
+#   policy_arn = aws_iam_policy.bronze_write_policy.arn
+# }
 
 resource "aws_iam_role_policy_attachment" "app_deploy_s3_frontend" {
   role       = aws_iam_role.app_deploy_role.name
